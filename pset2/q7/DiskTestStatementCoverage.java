eab5fa687ab087f5e0b8f7c11278589befa01c80
yap-yap-yap <39935719+flippinghell@users.noreply.github.com>
2021-03-02 02:01:50 +0800

    q7

---
+++ pset2/q7/DiskTestStatementCoverage.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+
+@RunWith(Parameterized.class)
+public class DiskTestStatementCoverage {
+    private int x;
+    private int y;
+    
+    public DiskTestStatementCoverage(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Parameters
+    public static Collection<Object[]> parameters(){
+        return Arrays.asList(new Object[][]{
+            {2, 0},
+            {1010, 0}
+        });
+    }
+
+    @Test
+    public void runDiskTest(){
+        Disk disk = new Disk(x, y);
+        try{
+            disk.manipulate();
+        }
+        catch(Exception exception){
+            fail("Exception caught");
+        }
+    }
+
+}
---
+++ pset2/q7/q7explanation.md
@@ -0,0 +1,1 @@
+A value of ``y < 1`` is sufficient to cover all statements that has ``y`` in it. However two values of ``x`` is needed to cover both statements that include ``y``. Thus two test cases is the minimum for full statement coverage.
